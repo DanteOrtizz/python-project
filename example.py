1c26c49a3493697ccb8ac09791d91b0d0151da6b
Dante O. <verlacdante@gmail.com>
2024-04-16 15:42:48 -0300

    config: Adding pylint to project

---
+++ example.py
@@ -0,0 +1,14 @@
+'''
+        Module doc
+'''
+
+def my_func():
+    '''
+        Doc for the function
+    '''
+    print('hi')
+
+
+# Pylint:
+# snake_case -> Functions, Variables, Methods
+# PascalCase -> Classes
